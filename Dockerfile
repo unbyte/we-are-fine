ad1662a203ce8885df506356ca67331094b81ecf
Helios <i@shangyes.net>
2020-03-21 01:29:57 +0800

    feat(init): now database will init during `docker build`

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:3.7
 COPY . /app
 WORKDIR /app
 RUN pip install --no-cache-dir -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+RUN python init-database.py
 EXPOSE 8080
 CMD ["python", "run-production.py"]
--- init-database.py
+++ init-database.py
@@ -2,12 +2,14 @@
 数据库初始化脚本。
 用于快速部署到新环境中。
 """
+import os
 
 from app import db, model
 
-# 初始化数据库模型
-model.initialize()
+if not os.path.exists("./data/database.db"):
+    # 初始化数据库模型
+    model.initialize()
 
-# 创建数据库的表结构。
-# 若已存在同名表，可能会出现失败问题。
-db.create_all()
+    # 创建数据库的表结构。
+    # 若已存在同名表，可能会出现失败问题。
+    db.create_all()
