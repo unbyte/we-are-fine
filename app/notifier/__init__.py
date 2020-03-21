9a91e1237dc4e082c586474c9fcc3667f465e2dc
Helios <i@shangyes.net>
2020-03-21 11:17:52 +0800

    refactor(config): now `host` and `port` are separate

--- app/notifier/__init__.py
+++ app/notifier/__init__.py
@@ -2,6 +2,7 @@ from app import app
 from .notifier import Notifier
 
 sender = Notifier(app.config["MAIL_HOST"],
+                  app.config["MAIL_PORT"],
                   app.config["MAIL_ACCOUNT"],
                   app.config["MAIL_PASSWORD"],
                   app.config["ADMIN_MAIL"],
