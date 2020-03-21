c7b4878a513c3694a7225f5358cc5328aa151ee1
Helios <i@shangyes.net>
2020-03-21 23:56:29 +0800

    chore: add cors in development mode for front-end development

--- run-development.py
+++ run-development.py
@@ -1,10 +1,13 @@
 """
 Development Server
 """
+from flask_cors import CORS
 
 from app import app
 
 if __name__ == '__main__':
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
     app.run(
         debug=app.config['DEBUG'],
         host=app.config['LISTEN_HOST_DEV'],
